7bc4d2a63172715b7485674683bd6863f195bcdd
Jakub <kubarak326@gmail.com>
2023-11-11 17:00:33 +0100

    anim (todo)

--- Source/Blaster/Character/BlasterAnimInstance.h
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -6,13 +6,28 @@
 #include "Animation/AnimInstance.h"
 #include "BlasterAnimInstance.generated.h"
 
+class ABlasterCharacter;
 /**
  * 
  */
 UCLASS()
 class BLASTER_API UBlasterAnimInstance : public UAnimInstance
 {
-	GENERATED_BODY();
+	GENERATED_BODY()
 public:
 	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category="Character", meta = (AllowPrivateAccess = "true"))
+	ABlasterCharacter* BlasterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 };
